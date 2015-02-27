e890ba52a50a5251493628ec7b331c43b37eda7f
Josh Rowe <josh@s-block.com>
2015-02-27 16:30:08 +0000

    don't force db_index=True

--- djorm_pgfulltext/fields.py
+++ djorm_pgfulltext/fields.py
@@ -4,13 +4,13 @@ import django
 from django.db import models
 from psycopg2.extensions import adapt
 
+
 class VectorField(models.Field):
     def __init__(self, *args, **kwargs):
         kwargs['null'] = True
         kwargs['default'] = ''
         kwargs['editable'] = False
         kwargs['serialize'] = False
-        kwargs['db_index'] = True
         super(VectorField, self).__init__(*args, **kwargs)
 
     def db_type(self, *args, **kwargs):
